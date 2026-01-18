3ad2af1ad584e8dee65b575dabd4d6a5c9c29756
Oualiddersaoui <oualiddersaoui@gmail.com>
2026-01-18 13:51:49 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+# INSTRUCTIONS FOR FRIENDS:
+# 1. Open SQL Server Management Studio (SSMS).
+# 2. Look at the "Server Name" in the Connect box.
+# 3. Paste that name below inside the quotes.
+
+DB_CONFIG = {
+    'driver': '{ODBC Driver 17 for SQL Server}',
+    'server': r'WALIDD\SQLEXPRESS',  # <--- THEY CHANGE THIS LINE ONLY
+    'database': 'DoctorServiceDB',
+    'trusted_connection': 'yes'
+}
